10104ba208d123d3f106abab1786c38ff72263da
baclayonwally-glitch <baclayonwally@gmail.com>
2026-05-21 16:16:42 +0800

    Update api.ts

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -2,24 +2,31 @@ import { RouteResponse } from "@/types/route";
 
 export async function fetchRoute(
   origin: [number, number],
-  destination: [number, number]
+  destination: [number, number],
+  model: string,
+  risk: number
 ): Promise<RouteResponse> {
+
   const response = await fetch(
     "https://flood-routing-api-1.onrender.com/predict_route",
     {
       method: "POST",
+
       headers: {
         "Content-Type": "application/json",
       },
+
       body: JSON.stringify({
         origin,
         destination,
+        model,
+        risk,
       }),
     }
   );
 
   if (!response.ok) {
-    throw new Error("No safe route found.");
+    throw new Error("No route found.");
   }
 
   return response.json();
