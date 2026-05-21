57cd4565b777e235627fd3849754486baf7fd34c
baclayonwally-glitch <baclayonwally@gmail.com>
2026-05-21 16:34:01 +0800

    Update route.ts

--- frontend/types/route.ts
+++ frontend/types/route.ts
@@ -1,9 +1,19 @@
 export interface RouteResponse {
   status: string;
+
   route: [number, number][];
+
   risk_edges: [[number, number], [number, number]][];
+
   distance_km: number;
+
   estimated_time_min: number;
+
   risk_score: number;
+
+  model_used: string;
+
+  risk_beta: number;
+
   message: string;
 }
